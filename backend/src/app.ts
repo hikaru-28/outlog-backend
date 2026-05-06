ff1aa05e63efd0b1c26d4eae048c191839e78db3
hikaru-28 <th_shine@icloud.com>
2026-05-06 10:39:46 +0900

    feat: CORS設定追加

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 import authRoutes from './routes/auth.routes';
 import inputRoutes from './routes/input.routes';
 import outputRoutes from './routes/output.routes';
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}))
+
 app.use(express.json());
-// CORSの設定
 app.use('/api/auth', authRoutes);
 app.use('/api/inputs', inputRoutes);
 app.use('/api/inputs/:inputId/output', outputRoutes);
