e2955d77e442f45de7ddcc02105154a4abc8c885
hikaru-28 <th_shine@icloud.com>
2026-05-13 11:23:14 +0900

    fix: 本番環境のCORS設定追加

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,7 +7,10 @@ import outputRoutes from './routes/output.routes';
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: [
+        'http://localhost:5173',
+        'https://outlog-frontend.vercel.app'
+    ],
     credentials: true
 }))
 
